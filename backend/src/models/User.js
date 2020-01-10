f629811927a1f7782a9bf33e7792e7db978736ff
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2020-01-10 09:31:20 -0300

    feat: Modelo da collection "User"

---
+++ backend/src/models/User.js
@@ -0,0 +1,40 @@
+const {Schema, model} = require('mongoose');
+const crypto = require('crypto');
+
+const UserSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false   // Não retorna a senha a menos que seja explicitado a seleção da mesma
+    },
+    description: {
+        type: String,
+        default: ''
+    }
+}, {
+    timestamps: true
+});
+
+// Cria o hash MD5 da senha antes de salvar no banco
+UserSchema.pre('save', async function(next) {
+    const hash = crypto.createHash('md5').update(this.password).digest('hex');
+    this.password = hash;
+
+    next()
+});
+
+module.exports = model('User', UserSchema);
