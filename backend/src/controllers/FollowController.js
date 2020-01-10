e282bec09987e9049da95b198fb8b2e64b66309a
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2020-01-10 09:34:26 -0300

    feat: Funções responsáveis pelas "ações" de "Follow"

---
+++ backend/src/controllers/FollowController.js
@@ -0,0 +1,66 @@
+const User = require('../models/User');
+const Following = require('../models/Following');
+
+module.exports = {
+    // FOLLOW UM USUÁRIO
+    async create(request, response) {
+        const userId = request.userId;
+        const {followingId} = request.params;
+
+        const followingExists = await User.findById(followingId);
+        const isFollowing = await Following.findOne({user: userId, following: followingId});
+
+        // Verifica se o usuário está tentado seguir a si mesmo
+        if (followingId == userId) {
+            return response.status(400).json({'error': 'You cannot follow yourself'});
+        }
+
+        // Verifica se o usuário que será seguido existe
+        if (!followingExists)
+            return response.status(400).json({'error': 'This user that you want to follow does not exist'});
+
+        // Verifica se o usuário já está seguindo o usuário selecionado
+        if (isFollowing) {
+            return response.status(400).json({'error': 'You are already following that user'});
+        }
+
+        try {
+            await Following.create({user: userId, following: followingId});
+
+        }
+        catch (error) {
+            return response.status(400).json({'error': error});
+        }
+
+        return response.status(204).end();  // Código 204 pois não retorna nenhum conteúdo
+    },
+
+    // UNFOLLOW UM USUÁRIO
+    async delete(request, response) {
+        const userId = request.userId;
+        const {followingId} = request.params;
+
+        const followingExists = await User.findById(followingId);
+        const isFollowing = await Following.findOne({user: userId, following: followingId});
+
+        // Verifica se o usuário que será seguido existe
+        if (!followingExists)
+            return response.status(400).json({'error': 'This user that you want to unfollow does not exist'});
+
+        // Verifica se o usuário realmente segue o usuário selecionado
+        if (!isFollowing) {
+            return response.status(400).json({'error': 'You are not following that user'});
+        }
+
+        // Tenta dar unfollow 
+        try {
+            await Following.findOneAndDelete({user: userId, following: followingId});
+
+        }
+        catch (error) {
+            return response.status(400).json({'error': error});
+        }
+
+        return response.status(204).end();  // Código 204 pois não retorna nenhum conteúdo
+    },
+}
