57d805144e731e0eadde4c1d661500c7055a71ff
VictorRodriguesDosReis <victor_reis005@hotmail.com>
2020-01-09 21:47:24 -0300

    feat: Criado a conexão com o mongoDB e adicionado arquivo das rotas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const server = express();
 
-server.get('/', function (request, response) {
-    response.send('teste');
+mongoose.connect('mongodb+srv://littlegram:senha@cluster0-qtrs8.gcp.mongodb.net/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
+
+server.use(express.json());
+server.use(routes);
 
 server.listen(3333);
